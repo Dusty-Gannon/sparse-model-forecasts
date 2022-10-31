bd1674ce58d8e6293369635a6044bf59f8b528f8
Dustin G Gannon <dgannon@tlog2.cluster>
2022-10-31 16:58:10 -0600

    running GLARMA threshold evals on teton

---
+++ Model_evals/run_pois_GLARMA-1-1_eval-thresh.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+
+#SBATCH --account=modelscape
+#SBATCH --time=12:00:00
+#SBATCH --nodes=21
+#SBATCH --ntasks-per-node=3
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=24G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=dgannon@uwyo.edu
+
+# Set the parameter combination to use and generate names of R scripts and log file
+Rscript=pois_GLARMA-1-1_eval-thresh.R
+LogFile=pois_GLARMA_thresh.log
+
+# Change to the relevant working directory
+cd /project/modelscape/analyses/sponges/Model_evals
+
+# Load R and MPI
+module load gcc/7.3.0 swset/2018.05 r-rstan/2.18.2-py27
+
+R CMD BATCH $Rscript $LogFile
+
