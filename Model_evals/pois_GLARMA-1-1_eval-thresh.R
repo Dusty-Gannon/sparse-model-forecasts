6f3deb405c8673b98ccbad520d3d479ca19c3bc5
Dusty-Gannon <gannondu@oregonstate.edu>
2022-11-03 15:09:17 -0600

    added figure summarizing results from fits to 500 times series with multiple values of the threshold parameter in a Poisson GLARMA(1,1) model

--- Model_evals/pois_GLARMA-1-1_eval-thresh.R
+++ Model_evals/pois_GLARMA-1-1_eval-thresh.R
@@ -2,6 +2,8 @@
 library(here)
 library(rstan)
 library(parallel)
+library(dplyr)
+library(tidyr)
 
 # # load functions on personal machine
 # devtools::load_all()
@@ -18,22 +20,30 @@ sapply(src_files, source, .GlobalEnv)
 # function to simulate from a GLARMA(1,1) process and fit the model with
 # 10 different fixed values of c and one random for augmenting the raw data
 
-threshold_eval <- function(X, stan_mod){
-  
+  # set parameter values
+  params = list(
+    n = 100,
+    beta = c(-0.5, 1, 0.5),
+    phi = 0.6,
+    theta = 0.5
+  )
+
+threshold_eval <- function(X, stan_mod, params){
+
   # handy function for posterior mode
 
   # simulating from a GLARMA(1,1) process
 
   # set parameters
     # time series length
-    n <- 100
+    n <- params$n
 
     # reg. coefficients
-    beta <- c(-0.5, 1, 0.5)
+    beta <- params$beta
 
     # AR and MA parameters
-    phi <- 0.6
-    theta <- 0.5
+    phi <- params$phi
+    theta <- params$theta
 
     # model matrix
     X <- matrix(
@@ -165,7 +175,7 @@ threshold_eval <- function(X, stan_mod){
   reps <- 500
 
   cl <- makeCluster(20)
-  
+
   clusterEvalQ(cl, {library(rstan)})
   clusterEvalQ(cl, {
     post_mode <- function(x){
@@ -178,12 +188,104 @@ threshold_eval <- function(X, stan_mod){
     cl = cl,
     X = 1:reps,
     fun = threshold_eval,
-    stan_mod = pois_garma11
+    stan_mod = pois_garma11,
+    params = params
   )
 
   stopCluster(cl)
 
 
- saveRDS(results, file = here("Data/Pois_GLARMA_thresh_eval/results_test.rds"))
+ saveRDS(results, file = here("Data/Pois_GLARMA_thresh_eval/pois_GLARMA_thresh_evals.rds"))
+
+
+
+
+#### Figure to visualize results ####
+  df_results <- data.frame(
+    c = rep(
+      c(seq(0.05, 0.95, length.out = 10), "random"),
+      length(results)
+    ),
+    phi = NA,
+    theta = NA,
+    beta0 = NA,
+    beta1 = NA,
+    beta2 = NA
+  )
+
+  # now loop through and add the appropriate values
+  for(i in 1:length(results)){
+
+    row_ids <- (11 * (i - 1) + 1):(11 * (i - 1) + 11)
+
+    df_results$phi[row_ids] <- results[[i]]$phi
+    df_results$theta[row_ids] <- results[[i]]$theta
+    df_results$beta0[row_ids] <- results[[i]]$beta[1, ]
+    df_results$beta1[row_ids] <- results[[i]]$beta[2, ]
+    df_results$beta2[row_ids] <- results[[i]]$beta[3, ]
+
+  }
+
+  # long format
+  df_results_long <- pivot_longer(
+    df_results,
+    cols = phi:beta2,
+    names_to = "param",
+    values_to = "estimate"
+  )
+
+  # summarize results for each threshold value
+  df_plot <- group_by(df_results_long, param, c) %>%
+    summarise(
+      estim = mean(estimate),
+      se = sd(estimate)
+    )
+
+  # create another dataframe with true values for each parameter
+  df_true <- data.frame(
+    param = unique(df_plot$param),
+    true = c(params$beta, params$phi, params$theta)
+  )
+
+  # create facet labels
+  param_labs <- c(
+    beta0 = "beta[0]",
+    beta1 = "beta[1]",
+    beta2 = "beta[2]",
+    phi = "phi",
+    theta = "theta"
+  )
+
+
+  # create the plot
+  p <- ggplot(data = df_plot, aes(x = c))+
+    geom_errorbar(aes(ymin = estim - se, ymax = estim + se))+
+    geom_point(aes(y = estim))+
+    geom_hline(
+      data = df_true, aes(yintercept = true),
+      color = "brown2", linetype = "dashed"
+    )+
+    theme_bw()+
+    theme(
+      axis.text.x = element_text(angle = 45, vjust = 0.5)
+    )+
+    xlab("Threshold value")+
+    ylab("Posterior mode")+
+    facet_wrap(
+      ~ param,
+      labeller = labeller(param = as_labeller(param_labs, label_parsed))
+    )
+
+  ggsave(
+    filename = here("Figures/Pois_GLARMA_threshold_evals.png"),
+    plot = p, width = 6, height = 4, units = "in",
+    device = "png"
+  )
+
+
+
+
+
+
 
 
