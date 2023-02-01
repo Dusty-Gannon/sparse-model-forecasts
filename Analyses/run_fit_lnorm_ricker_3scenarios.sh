01de9a7e624913a60e14b8fa68d52ea387e9847b
Dusty-Gannon <gannondu@oregonstate.edu>
2023-02-01 14:32:21 -0700

    not sure if parallel package is working on beartooth. try executing in tandem as well

--- Analyses/run_fit_lnorm_ricker_3scenarios.sh
+++ Analyses/run_fit_lnorm_ricker_3scenarios.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -l
 
 #SBATCH --account=modelscape
-#SBATCH --time=48:00:00
+#SBATCH --time=72:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=21
+#SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=32G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=dgannon@uwyo.edu
 
 # Set the parameter combination to use and generate names of R scripts and log file
-Rscript=fit_lnorm_ricker_3scenarios.R
+Rscript=fit_lnorm_ricker_3scenarios_tandem.R
 LogFile=ricker_vrtests
 
 # Change to the relevant working directory
@@ -19,4 +19,4 @@ cd /project/modelscape/analyses/sponges/Analyses
 # Load R and MPI
 module load arcc/1.0  gcc/12.2.0 r/4.2.2
 
-Rscript --vanilla $Rscript lnorm_ricker_sims_200steps_rho0_S3_s57.rds 20 > $LogFile
+Rscript --vanilla $Rscript lnorm_ricker_sims_200steps_rho0_S3_s57.rds > $LogFile
