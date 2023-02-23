3da6eb08b0d691e8e9ca1bc6b740e726386255fc
Dusty-Gannon <gannondu@oregonstate.edu>
2023-02-23 16:35:24 -0700

    Move some of the lnorm Ricker sim functions in the the documented R directory

--- R/terrestrial_ricker_sim_functions.R
+++ R/terrestrial_ricker_sim_functions.R
@@ -200,6 +200,150 @@ ricker_step_pois <- function(N_t, lambdas, A_mat, stochastic = T){
 }
 
 
+
+
+
+
+
+
+
+#' Generate a list of simulation parameters for lognormal Ricker model
+#'
+#' @param x Dummy variable so that this function is straight forward to use with apply statements
+#' @param nsp Number of species to start with
+#' @param steps Number of steps over which to simulate
+#' @param num_ngs Number of non-generic species
+#' @param sigma_rng Interval over which to draw scales of demographic stochasticity
+#' @param alpha_rng Interval from which to draw intraspecific competition coefficients
+#' @param lambda_rng Interval from which to draw low-density growth rates
+#' @param ng_range Range over which to draw the fraction of intra-specific growth for the non-generics
+#' (i.e., 0.5 means that non-generic competition coefficient would be half as large as intra-specific
+#' competition)
+#' @param rho Fraction of intraspecific competition that is the generic competitive effect
+#' @param mean_init_abund Average initial abundance for the random initial states
+#' @param comp_matrix_type Non-generic competition can either be random (\code{comp_matrix_type = 1})
+#' or structured so that no species gets overloaded with strong competitors (\code{comp_matrix_type = 2}).
+#' @param het_vr Multiplier by which to amplify the variance of heterospecific abundances.
+#'
+#' @return List of parameters for the simulation
+#'
+generate_sim_params_vrtests <- function(
+    x, nsp = 40, steps = 200, num_ngs = 3,
+    sigma_rng = c(0.1, 0.5), alpha_rng = c(0.005, 0.01),
+    lambda_rng = c(1.2, 1.8), ng_range = c(0.2, 0.4), rho = 0,
+    mean_init_abund = 20, comp_matrix_type = 2, het_vr = 1
+){
+
+  # generate competition matrix
+  alpha <- runif(nsp, min = alpha_rng[1], max = alpha_rng[2])
+  if(comp_matrix_type == 1){
+    A_mat <- sponges::comp_matrix(
+      n_sp = nsp, rho = rho, alpha = alpha,
+      num_ngs = num_ngs, ng_range = ng_range
+    )
+  }
+  if(comp_matrix_type == 2){
+    A_mat <- sponges::comp_matrix2(
+      n_sp = nsp, rho = rho, alpha = alpha,
+      num_ngs = num_ngs, ng_range = ng_range
+    )
+  }
+
+  # compile list of return objects
+  return(
+    list(
+      nsp = nsp,
+      steps = steps,
+      sigmas = runif(nsp, min = sigma_rng[1], max = sigma_rng[2]),
+      A_mat = A_mat,
+      lambdas = runif(nsp, min = lambda_rng[1], max = lambda_rng[2]),
+      N0 = rpois(nsp, lambda = mean_init_abund),
+      het_vr = het_vr
+    )
+  )
+
+}
+
+
+
+
+
+
+
+
+
+#' Generate simulation parameters for single TS thinning experiments
+#'
+#' @param x Dummy variable so that this function is straight forward to use with apply statements
+#' @param nsp Number of species to start with
+#' @param init_steps Number of steps before thinning
+#' @param tot_steps Total number of steps over which to simulate
+#' @param num_ngs Number of non-generic species
+#' @param sigma_rng Interval over which to draw scales of demographic stochasticity
+#' @param alpha_rng Interval from which to draw intraspecific competition coefficients
+#' @param lambda_rng Interval from which to draw low-density growth rates
+#' @param ng_range Range over which to draw the fraction of intra-specific growth for the non-generics
+#' (i.e., 0.5 means that non-generic competition coefficient would be half as large as intra-specific
+#' competition)
+#' @param rho Fraction of intraspecific competition that is the generic competitive effect
+#' @param mean_init_abund Average initial abundance for the random initial states
+#' @param comp_matrix_type Non-generic competition can either be random (\code{comp_matrix_type = 1})
+#' or structured so that no species gets overloaded with strong competitors (\code{comp_matrix_type = 2}).
+#' @param thin_freq Frequency of thinning treatments. \code{thin_freq = 0} will proceed without
+#' thinning treatments, while a value of \code{thin_freq = c} with c > 0, will perform a
+#' thinning treatment every c years.
+#' @param thin_factor Factor by which to thin a species. \code{thin_freq = 0.1} with thin a
+#' species to 10 percent of its population density in the previous year.
+#'
+#' @return List of simulation parameters
+#'
+generate_sim_params_thin <- function(
+    x, nsp = 40, init_steps = 100, tot_steps = 500, num_ngs = 3,
+    sigma_rng = c(0.1, 0.5), alpha_rng = c(0.005, 0.01),
+    lambda_rng = c(1.2, 1.8), ng_range = c(0.2, 0.4), rho = 0,
+    mean_init_abund = 20, comp_matrix_type = 2, thin_freq = 2,
+    thin_factor = 0.1, target_thin = TRUE
+){
+
+  # generate competition matrix
+  alpha <- runif(nsp, min = alpha_rng[1], max = alpha_rng[2])
+  if(comp_matrix_type == 1){
+    A_mat <- sponges::comp_matrix(
+      n_sp = nsp, rho = rho, alpha = alpha,
+      num_ngs = num_ngs, ng_range = ng_range
+    )
+  }
+  if(comp_matrix_type == 2){
+    A_mat <- sponges::comp_matrix2(
+      n_sp = nsp, rho = rho, alpha = alpha,
+      num_ngs = num_ngs, ng_range = ng_range
+    )
+  }
+
+  # compile list of return objects
+  return(
+    list(
+      nsp = nsp,
+      init_steps = init_steps, tot_steps = tot_steps,
+      sigmas = runif(nsp, min = sigma_rng[1], max = sigma_rng[2]),
+      A_mat = A_mat,
+      lambdas = runif(nsp, min = lambda_rng[1], max = lambda_rng[2]),
+      N_0 = rpois(nsp, lambda = mean_init_abund),
+      thin_factor = thin_factor, thin_freq = thin_freq,
+      target_thin = target_thin
+    )
+  )
+
+}
+
+
+
+
+
+
+
+
+
 #' Simulating a Ricker population model with log-normal demographic stochasticity
 #'
 #' @param N_0 Initial abundance of the focal species
@@ -229,4 +373,90 @@ ricker_ts_lnorm_foc <- function(N_0, lambda, A_i, sigma_i, N_het, steps = 300, h
 
 
 
+
+
+
+#' Full community Ricker simulations
+#'
+#' @param N_0 Vector of inital population abundances
+#' @param lambdas Vector of low-density growth rates
+#' @param A_mat Competition matrix
+#' @param sigmas Vector of scales of demographic stochasticity
+#' @param steps Number of time steps
+#' @param thin_freq Frequency of thinning treatments. \code{thin_freq = 0} will proceed without
+#' thinning treatment, while a value of \code{thin_freq = c} with c > 0, will perform a
+#' thinning treatment every c years.
+#' @param thin_factor Factor by which to thin a species. \code{thin_freq = 0.1} with thin a
+#' species to 10 percent of its population density in the previous year.
+#' @param thin_order Order in which to thin species. If \code{NULL}, the order will be
+#' generated at random
+#'
+#' @return Matrix with as many rows as there are species in the simulation and as many columns
+#' as steps.
+#'
+#'
+ricker_ts_lnorm <- function(N_0, lambdas, A_mat, sigmas, steps, thin_freq = 0, thin_factor = 0.1, thin_order = NULL){
+
+  nsp <- length(N_0)
+  # initialize tracking matrix
+  N <- matrix(0, nrow = nsp, ncol = steps)
+  N[, 1] <- N_0
+
+  if(thin_freq == 0){
+    for(t in 2:steps){
+
+      # tracking extinct species
+      extinct <- which(round(as.double(N[, t - 1])) == 0)
+      N[extinct, t - 1] <- 0
+      nesp <- (1:nsp)[-extinct]
+      if(length(extinct) > 0){
+        N[nesp, t] <- N[nesp, t - 1] * lambdas[nesp] *
+          exp(- A_mat[nesp, nesp] %*% N[nesp, t - 1] + rnorm(length(nesp)) * sigmas[nesp] / sqrt(N[nesp, t - 1]))
+      } else{
+        N[, t] <- N[, t - 1] * lambdas * exp(- A_mat %*% N[, t - 1] + rnorm(nsp) * sigmas / sqrt(N[, t - 1]))
+      }
+
+    }
+  }
+
+  if(thin_freq > 0){
+
+    # create vector of thinning factors
+    thin <- rep(1, steps)
+    thin[thin_freq * c(1:floor(steps/thin_freq))] <- thin_factor
+
+    # create vector that cycles through the thinning order
+    if(is.null(thin_order)){thin_order <- sample(1:nsp)}
+    sp2thin <- rep(
+      rep(thin_order, each = thin_freq),
+      ceiling(steps / (thin_freq * nsp))
+    )
+
+    # now proceed with simulations
+    for(t in 2:steps){
+
+      # tracking extinct species
+      extinct <- which(round(as.double(N[, t - 1])) == 0)
+      N[extinct, t - 1] <- 0
+
+      # thin species if it was a thinning year
+      N[sp2thin[t - 1], t - 1] <- N[sp2thin[t - 1], t - 1] * thin[t - 1]
+
+      # step the process forward
+      nesp <- (1:nsp)[-extinct]
+      if(length(extinct) > 0){
+        N[nesp, t] <- N[nesp, t - 1] * lambdas[nesp] *
+          exp(- A_mat[nesp, nesp] %*% N[nesp, t - 1] + rnorm(length(nesp)) * sigmas[nesp] / sqrt(N[nesp, t - 1]))
+      } else{
+        N[, t] <- N[, t - 1] * lambdas * exp(- A_mat %*% N[, t - 1] + rnorm(nsp) * sigmas / sqrt(N[, t - 1]))
+      }
+
+    }
+
+  }
+
+  return(N)
+
+
+}
 
