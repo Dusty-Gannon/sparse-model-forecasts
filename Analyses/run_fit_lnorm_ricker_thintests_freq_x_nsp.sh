eb28b1f1c5175bc71f91cce0aaa71b3184eb1618
Dusty-Gannon <gannondu@oregonstate.edu>
2023-03-08 10:48:50 -0700

    fix indexing problem and convert to array submission on beartooth

--- Analyses/run_fit_lnorm_ricker_thintests_freq_x_nsp.sh
+++ Analyses/run_fit_lnorm_ricker_thintests_freq_x_nsp.sh
@@ -1,22 +1,34 @@
 #!/bin/bash -l
 
 #SBATCH --account=modelscape
-#SBATCH --time=72:00:00
+#SBATCH --time=06:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=21
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=32G
+#SBATCH --mem=8G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=dgannon@uwyo.edu
+#SBATCH --array=1-101
 
 # Set the parameter combination to use and generate names of R scripts and log file
 Rscript=fit_lnorm_ricker_mods_parallel.R
-LogFile=ricker_thin2
+LogFile_pref=ricker_thin2_
 
 # Change to the relevant working directory
 cd /project/modelscape/analyses/sponges/Analyses
 
-# Load R
+# Load modules
 module load arcc/1.0  gcc/12.2.0  r/4.2.2
 
-Rscript --vanilla $Rscript lnorm_ricker_thin_freq_x_nsp_ordered_S5_s55.rds 51 500 freq_x_nsp_thintests_mfits.rds > $LogFile
+# create arrays of arguments
+start=()
+stop=()
+for i in ${SLURM_ARRAY_TASK_ID[@]}; do
+  start+=$(( 100*(i-1)+1 ))
+  stop+=$(( 100*i ))
+done
+
+Rscript --vanilla $Rscript lnorm_ricker_thin_freq_x_nsp_ordered_S5_s55.rds 51 500 ${start} ${stop} "freq_x_nsp_thintests_${start}_${stop}.rds" > "$LogFile_pref${start}_${stop}"
+
+
+
