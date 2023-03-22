21f39d94adaf1c1f1cafe55321f2905188303bd5
Dustin G Gannon <dgannon@blog2.cluster>
2023-03-22 11:56:04 -0600

    v2 wrapper changes. Committed by Dusty for Alice.

--- Simulations/run_ARp_beta_model_v2_wrapper.sh
+++ Simulations/run_ARp_beta_model_v2_wrapper.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
 
 # Set the parameter combination to use and generate names of R scripts and log file
-OutDir=ARp_sims_variable_sigmas_steps_0beta
+OutDir=ARp_sims_variable_sigmas_steps
 #nsteps=370
 #sigma=1
 module load arcc/1.0 gcc/12.2.0 r/4.2.2
 
-sbatch --wait run_ARp_beta_model_v2.sh $OutDir
+#sbatch --wait run_ARp_beta_model_v2.sh $OutDir
 
 # define the second R script that will be run
 Rscript=combine_ARp_sim_output.R
