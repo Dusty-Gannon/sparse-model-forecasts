96d31b75299c95e24b06a697f0386e31779eedf3
Dusty-Gannon <gannondu@oregonstate.edu>
2023-03-22 09:38:39 -0600

    try limiting number of tasks running simultaneously to see if it helps with memory usage

--- Analyses/run_fit_lnorm_ricker_thintests_freq_x_nsp.sh
+++ Analyses/run_fit_lnorm_ricker_thintests_freq_x_nsp.sh
@@ -3,12 +3,13 @@
 #SBATCH --account=modelscape
 #SBATCH --time=01:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=26
+#SBATCH --ntasks-per-node=21
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=20G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=dgannon@uwyo.edu
-#SBATCH --array=1-2020
+#SBATCH --array=1-2525%10
+#SBATCH --out=./logfiles/slurm-%A_%a.out
 
 # Set the parameter combination to use and generate names of R scripts and log file
 Rscript=fit_lnorm_ricker_mods_parallel.R
@@ -24,8 +25,8 @@ module load arcc/1.0  gcc/12.2.0  r/4.2.2
 start=()
 stop=()
 for i in ${SLURM_ARRAY_TASK_ID[@]}; do
-  start+=$(( 25*(i-1)+1 ))
-  stop+=$(( 25*i ))
+  start+=$(( 20*(i-1)+1 ))
+  stop+=$(( 20*i ))
 done
 
 Rscript --vanilla $Rscript lnorm_ricker_thin_freq_x_nsp_ordered_S5_s55.rds 51 150 ${start} ${stop} "freq_x_nsp_thintests_${start}_${stop}.rds" > "$LogFile_pref${start}_${stop}"
