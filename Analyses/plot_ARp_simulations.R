99ba72db361425e1b9ac0490dca7cdd3f6019b5c
Alice Carter <alicecarter05@gmail.com>
2023-04-26 13:16:02 -0600

    updated figures based on most recent sim run

--- Analyses/plot_ARp_simulations.R
+++ Analyses/plot_ARp_simulations.R
@@ -6,16 +6,18 @@
 
 library(tidyverse)
 dd <- read_csv('Data/aquatic_sim_data/AR_p_sims_model_output_condensed.csv')
+dd <- read_csv('Data/aquatic_sim_data/AR_p_sims_loosened_sig_more_burnin_4_12_condensed.csv')
 
 dat <- dd %>%
   # filter(divergent_trans < 20) %>%
   mutate(siglab = factor(paste0('sigma = ', sigma),
-                         levels = c('sigma = 10', 'sigma = 1', 'sigma = 0.1'))) %>%
+                         levels = c('sigma = 5', 'sigma = 1', 'sigma = 0.2'))) %>%
   select(-rmse_sigma, -unconverged_pars, -divergent_trans) %>%
   pivot_longer(cols = starts_with('rmse'),
                names_to = 'parameter', values_to = 'rmse',
                names_pattern = 'rmse_([a-z]+)') %>%
-  mutate(parameter = factor(parameter, levels = c('forecast', 'phi', 'beta')))
+  mutate(parameter = factor(parameter, levels = c('forecast', 'phi', 'beta')))%>%
+  filter(!is.na(siglab))
 datr <- filter(dat, model == 'reg')
 datnr <- filter(dat, model == 'not_reg')
 # not all simulations ran - that is something that needs to be figured out later
@@ -46,7 +48,7 @@ lgnd <- ggpubr::get_legend(dummy_plot)
 pp <- ggpubr::ggarrange(p, legend.grob = lgnd)
 
 png(width = 10.5, height = 9, units = 'in', type = 'cairo', res = 300,
-    filename = 'Figures/ARp_sim_fits.png')
+    filename = 'Figures/ARp_sim_fits2.png')
     ggpubr::annotate_figure(pp, top = ggpubr::text_grob('AR-p simulation model fits'))
 dev.off()
 
@@ -60,14 +62,20 @@ lines(density(dd$n[dat$sigma == 10]), col = 3)
 
 library(RColorBrewer)
 dd %>%
+  filter(divergent_trans < 20) %>%
   mutate(sigma = factor(sigma))%>%
-  group_by(n, sigma) %>%
+  group_by(n, sigma, model) %>%
   summarize(nsims = n()/2) %>%
   ggplot(aes(factor(n), nsims, fill = sigma))+
   geom_bar(position = 'dodge', stat = 'identity') +
   scale_fill_brewer(palette = 'Blues')+
+  facet_wrap(.~model)+
   ylab('Number of simulations') + xlab('Time series length')+
   theme_minimal()
 
 
 # quantify divergent transitions
+dd %>% group_by(n, sigma, model) %>%
+  summarize(min_div = min(divergent_trans),
+    median_div = median(divergent_trans),
+            count = n())
