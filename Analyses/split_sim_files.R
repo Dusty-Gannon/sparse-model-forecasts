93847f18bf245281e8e523c94dc4a0d6389a74da
Dusty-Gannon <gannondu@oregonstate.edu>
2023-07-25 14:00:37 -0600

    file splitting script

---
+++ Analyses/split_sim_files.R
@@ -0,0 +1,44 @@
+########################################################
+# This script is meant to run on Beartooth to split
+# simulation data files that contain lists of results
+# from replicate simulations into separate files for each
+# result. This is handy for fitting with array jobs.
+# **NOTE**: The data files must contain the same number
+# of results for the numbering to make sense and not
+# overwrite files.
+########################################################
+
+library(here)
+
+# Arguments include the path to the directory with the
+# data files that are to be split and, optionally, an
+# output directory. If no output is supplied, the files
+# are stored in the input directory
+args <- commandArgs(trailingOnly = T)
+if(length(args) == 1){
+  args <- c(args, args)
+}
+
+file_list <- list.files(args[1])
+
+# loop through and create unique numbering and save
+# unique files
+for(i in 1:length(file_list)){
+  dat_i <- readRDS(here(paste0(args[1], file_list[i])))
+  for(j in 1:length(dat_i)){
+    dat_ij <- dat_i[[j]]
+    name_ij <- paste(
+      sub("_rep.*", "", x = file_list[i]),
+      (length(dat_i) * (i - 1)) + j,
+      sep = "_"
+    )
+    saveRDS(
+      dat_ij,
+      file = paste0(
+        args[2],
+        name_ij,
+        ".rds"
+      )
+    )
+  }
+}
