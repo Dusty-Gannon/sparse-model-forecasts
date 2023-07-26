22c9f70410431c34d41225c7d35417819bc755af
Alice Carter <acarte26@blog2.cluster>
2023-07-26 12:08:09 -0600

    committing before branch change on beartooth

--- Analyses/run_fit_lnorm_ricker_dist_freq_x_nsp_x_int_s4t.sh
+++ Analyses/run_fit_lnorm_ricker_dist_freq_x_nsp_x_int_s4t.sh
@@ -8,7 +8,7 @@
 #SBATCH --mem=8G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=dgannon@uwyo.edu
-#SBATCH --array=1-10%10
+#SBATCH --array=11-5000%10
 #SBATCH --out=./logfiles/slurm-%A_%a.out
 
 # Set the parameter combination to use and generate names of R scripts and log file
