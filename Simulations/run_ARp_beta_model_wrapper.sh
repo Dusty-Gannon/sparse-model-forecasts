4e42dbc9fedefdb33b530520f25bafc1e853ff85
Alice Carter <acarte26@blog2.cluster>
2023-07-26 12:38:41 -0600

    compiled simulations from AR err model run

--- Simulations/run_ARp_beta_model_wrapper.sh
+++ Simulations/run_ARp_beta_model_wrapper.sh
@@ -5,12 +5,12 @@ OutDir='ARp_err_sims_7_20'
 
 module load arcc/1.0 gcc/12.2.0 r/4.2.2
 
-sbatch --wait run_ARp_beta_model.sh $OutDir
+#sbatch --wait run_ARp_beta_model.sh $OutDir
 
 # define the second R script that will be run
-#Rscript=combine_ARp_sim_output.R
+Rscript=combine_ARp_sim_output.R
 
-#Rscript --vanilla $Rscript $OutDir
+Rscript --vanilla $Rscript $OutDir
 
 # remove directory of individual files
 #rm -r /project/modelscape/analyses/sponges/Data/aquatic_sim_data/$OutDir
