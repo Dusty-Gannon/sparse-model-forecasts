659cca14df76c0a5c8dcf82d1a19e43c4bf1da6f
aestears <astears@uwyo.edu>
2023-09-11 16:42:15 -0600

    make file for AIC model selection code

---
+++ Simulations/AIC_modelSelection.R
@@ -0,0 +1,14 @@
+#////////////
+# Simulates times series, fits basic OLS linear model to them, and then does AIC model selection
+#///////////
+
+
+# Step 1: Simulate time series --------------------------------------------
+
+
+# Step 2: Fit linear model ------------------------------------------------
+
+
+# Do AIC model selection --------------------------------------------------
+
+
