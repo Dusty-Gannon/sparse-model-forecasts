266de2be8eb39fc1717d814affbf056a38477616
Amy Patterson <amypatterson@Amys-MacBook-Pro.local>
2024-04-22 14:05:54 -0600

    stepwise vs step-wise correction, and updated parameter definitions

--- R/AICvsSTANfigures.R
+++ R/AICvsSTANfigures.R
@@ -26,17 +26,17 @@ corData6=corData5[-which(corData5$corrLevel==0.7),]
 par(mar=c(5,9,2,2))
 par(mfrow=c(3,1))
 vioplot::vioplot(cbind(AIC5=corData6$TPRaic[which(corData6$corrLevel==0.5)],AIC9=corData6$TPRaic[which(corData6$corrLevel==0.9)],STAN5=corData6$TPRstan[which(corData6$corrLevel==0.5)],STAN9=corData6$TPRstan[which(corData6$corrLevel==0.9)]), xlab="",
-                 horizontal=T,las=1,names=c("Step-wise AIC\n correlation 0.5","Step-wise AIC\n correlation 0.9","Horseshoe\n correlation 0.5","Horseshoe\n correlation 0.9"),col=c("steelblue1","steelblue4","tan","tan3"),
+                 horizontal=T,las=1,names=c("Stepwise AIC\n correlation 0.5","Stepwise AIC\n correlation 0.9","Horseshoe\n correlation 0.5","Horseshoe\n correlation 0.9"),col=c("steelblue1","steelblue4","tan","tan3"),
                  pchMed=20,border=c("royalblue4","royalblue4","tan4","tan4"),rectCol=c("royalblue4","royalblue4","tan4","tan4"), lineCol=c("royalblue4","royalblue4","tan4","tan4"),colMed=c("royalblue4","royalblue4","tan4","tan4"),ylab="")
 title(ylab="Density of TPR",line=7,cex.lab=1.2)
 title(xlab="TPR",line=3,cex.lab=1.2)
 vioplot::vioplot(cbind(AIC5=corData6$TNRaic[which(corData6$corrLevel==0.5)],AIC9=corData6$TNRaic[which(corData6$corrLevel==0.9)],STAN5=corData6$TNRstan[which(corData6$corrLevel==0.5)],STAN9=corData6$TNRstan[which(corData6$corrLevel==0.9)]), xlab="",
-                 horizontal=T,las=1,names=c("Step-wise AIC\n correlation 0.5","Step-wise AIC\n correlation 0.9","Horseshoe\n correlation 0.5","Horseshoe\n correlation 0.9"),col=c("steelblue1","steelblue4","tan","tan3"),
+                 horizontal=T,las=1,names=c("Stepwise AIC\n correlation 0.5","Stepwise AIC\n correlation 0.9","Horseshoe\n correlation 0.5","Horseshoe\n correlation 0.9"),col=c("steelblue1","steelblue4","tan","tan3"),
                  pchMed=20,border=c("royalblue4","royalblue4","tan4","tan4"),rectCol=c("royalblue4","royalblue4","tan4","tan4"), lineCol=c("royalblue4","royalblue4","tan4","tan4"),colMed=c("royalblue4","royalblue4","tan4","tan4"),ylab="")
 title(ylab="Density of TNR",line=7,cex.lab=1.2)
 title(xlab="TNR",line=3,cex.lab=1.2)
 vioplot::vioplot(cbind(AIC5=corData6$RMSEaic[which(corData6$corrLevel==0.5)],AIC9=corData6$RMSEaic[which(corData6$corrLevel==0.9)],STAN5=corData6$RMSEstan[which(corData6$corrLevel==0.5)],STAN9=corData6$RMSEstan[which(corData6$corrLevel==0.9)]), xlab="",
-                 horizontal=T,las=1,names=c("Step-wise AIC\n correlation 0.5","Step-wise AIC\n correlation 0.9","Horseshoe\n correlation 0.5","Horseshoe\n correlation 0.9"),col=c("steelblue1","steelblue4","tan","tan3"),
+                 horizontal=T,las=1,names=c("Stepwise AIC\n correlation 0.5","Stepwise AIC\n correlation 0.9","Horseshoe\n correlation 0.5","Horseshoe\n correlation 0.9"),col=c("steelblue1","steelblue4","tan","tan3"),
                  pchMed=20,border=c("royalblue4","royalblue4","tan4","tan4"),rectCol=c("royalblue4","royalblue4","tan4","tan4"), lineCol=c("royalblue4","royalblue4","tan4","tan4"),colMed=c("royalblue4","royalblue4","tan4","tan4"),ylab="")
 title(ylab="Density of Prediction RMSE",line=7,cex.lab=1.2)
 title(xlab="Prediction RMSE",line=3,cex.lab=1.2)
@@ -44,11 +44,11 @@ title(xlab="Prediction RMSE",line=3,cex.lab=1.2)
 ################################################
 # Figure 3
 ################################################
-corData2=read.csv(file="Simulations/AICvsSTANdecorrelation.csv")
+decorData=read.csv(file="Simulations/AICvsSTANdecorrelation.csv")
 
 
 par(mar=c(5,9,2,2))
 par(mfrow=c(1,1))
-vioplot::vioplot(cbind(AICdecorr=corData2$RMSEaic[1:1800],AICnorm=corData2$RMSEaic[1801:3600],STANdecorr=corData2$RMSEstan[1:1800],STANnorm=corData2$RMSEstan[1801:3600]),las=1,cex.axis=0.9,names=c("Step-wise AIC\n decorrelated","Step-wise AIC\n normal","Horseshoe\n decorrelated","Horseshoe\n normal"),
+vioplot::vioplot(cbind(AICdecorr=decorData$RMSEaic[1:1800],AICnorm=decorData$RMSEaic[1801:3600],STANdecorr=decorData$RMSEstan[1:1800],STANnorm=decorData$RMSEstan[1801:3600]),las=1,cex.axis=0.9,names=c("Stepwise AIC\n decorrelated","Stepwise AIC\n normal","Horseshoe\n decorrelated","Horseshoe\n normal"),
                  xlab="",horizontal=T,col=c("steelblue1","steelblue4","tan","tan3"),pchMed=20,border=c("royalblue4","royalblue4","tan4","tan4"),rectCol=c("royalblue4","royalblue4","tan4","tan4"),
                  lineCol=c("royalblue4","royalblue4","tan4","tan4"),colMed=c("royalblue4","royalblue4","tan4","tan4"),ylab="")
