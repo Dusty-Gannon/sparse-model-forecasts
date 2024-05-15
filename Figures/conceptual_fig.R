b12356474500bce2413b1ba4a5c85ae5a1c72522
Dusty-Gannon <gannondu@oregonstate.edu>
2024-05-15 12:00:16 -0700

    begin work on conceptual figure number 2

--- Figures/conceptual_fig.R
+++ Figures/conceptual_fig.R
@@ -35,42 +35,81 @@ df <- data.frame(
   time = 1:N
 )
 
+# get weights using ols
+w <- lm(df$y ~ decomp)$coefficients
+
 df <- cbind(df, as.data.frame(decomp))
+
+theme <- theme_classic() +
+  theme(
+    axis.title.y = element_text(angle = 0, vjust = 0.5)
+  )
 
 raw <- ggplot(df, aes(x = time)) +
   geom_line(aes(y = y)) +
-  theme_classic() +
+  theme +
   xlab("")
+
 
 s1 <- ggplot(df, aes(x = time, y = `S1-100`)) +
   geom_line() +
-  theme_classic() +
+  theme +
   xlab("") +
   ylab(expression(w[1]))
 
 c1 <- ggplot(df, aes(x = time, y = `C1-100`)) +
   geom_line() +
-  theme_classic() +
+  theme +
+  ylab(expression(w[2])) +
   xlab("")
 
-s2 <- ggplot(df, aes(x = time, y = `S2-100`)) +
+# now figure out which signals have largest weights
+large <- names(sort(abs(w[-1]), decreasing = T)[1:2])
+small <- names(sort(abs(w[-1]))[1:2])
+
+large_ids <- which(names(w) %in% large)
+small_ids <- which(names(w) %in% small)
+
+s3 <- ggplot(df, aes(x = time, y = `S3-100`)) +
   geom_line() +
-  theme_classic() +
+  theme +
+  ylab(expression(w[6])) +
   xlab("")
 
-c2 <- ggplot(df, aes(x = time, y = `C2-100`)) +
+s11 <- ggplot(df, aes(x = time, y = `S11-100`)) +
   geom_line() +
-  theme_classic() +
+  theme +
+  ylab(expression(w[22])) +
   xlab("")
 
-s20 <- ggplot(df, aes(x = time, y = `S20-100`)) +
+s32 <- ggplot(df, aes(x = time, y = `S32-100`)) +
   geom_line() +
-  theme_classic() +
+  theme +
+  ylab(expression(w[64])) +
   xlab("")
 
-c20 <- ggplot(df, aes(x = time, y = `C20-100`)) +
+c41 <- ggplot(df, aes(x = time, y = `C41-100`)) +
   geom_line() +
-  theme_classic()
+  theme +
+  ylab(expression(w[83]))
 
-(panel1 <- (raw / s1 / c1 / s2 / c2 / s20 / c20))
+
+(panel1 <- (raw / s1 / c1 / s3 / s11 / s32 / c41))
+
+# start second panel with linear combos
+
+wids <- which(
+  names(w) %in%
+    c("decompS1-100", "decompC1-100", "decompS3-100",)
+)
+
+
+
+
+
+
+
+
+
+
 
