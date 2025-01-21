5830b62567edda0e564973e5b16e2f8ee0076ba1
apatt76 <amy.patterson76@gmail.com>
2025-01-21 13:39:46 -0700

    labels for challenge 1 figure

--- Figures/AICvsSTANfigures.R
+++ Figures/AICvsSTANfigures.R
@@ -105,16 +105,25 @@ vioplot::vioplot(cbind(AIC1=corData6$TPRaic[which(corData6$corrLevel==0.1)],STAN
                  pchMed=20,border=c("royalblue4","royalblue4","royalblue4","tan4","tan4","tan4"),rectCol=c("royalblue4","royalblue4","royalblue4","tan4","tan4","tan4"), lineCol=c("royalblue4","royalblue4","royalblue4","tan4","tan4","tan4"),colMed=c("royalblue4","royalblue4","royalblue4","tan4","tan4","tan4"),ylab="")
 title(ylab="Density of TPR",line=7,cex.lab=1.2)
 title(xlab="True Positive Rate (TPR)",line=3,cex.lab=1.2)
+par(xpd=T)
+text(-0.02,7,"a)",cex=1.5)
+par(xpd=F)
 vioplot::vioplot(cbind(AIC1=corData6$TNRaic[which(corData6$corrLevel==0.1)],STAN1=corData6$TNRstan[which(corData6$corrLevel==0.1)],AIC5=corData6$TNRaic[which(corData6$corrLevel==0.5)],STAN5=corData6$TNRstan[which(corData6$corrLevel==0.5)],AIC9=corData6$TNRaic[which(corData6$corrLevel==0.9)],STAN9=corData6$TNRstan[which(corData6$corrLevel==0.9)]), xlab="",
                  horizontal=T,las=1,names=c("Stepwise AIC\n correlation 0.1","Horseshoe\n correlation 0.1","Stepwise AIC\n correlation 0.5","Horseshoe\n correlation 0.5","Stepwise AIC\n correlation 0.9","Horseshoe\n correlation 0.9"),col=c("skyblue","steelblue1","steelblue4","wheat","tan","tan3"),
                  pchMed=20,border=c("royalblue4","royalblue4","royalblue4","tan4","tan4","tan4"),rectCol=c("royalblue4","royalblue4","royalblue4","tan4","tan4","tan4"), lineCol=c("royalblue4","royalblue4","royalblue4","tan4","tan4","tan4"),colMed=c("royalblue4","royalblue4","royalblue4","tan4","tan4","tan4"),ylab="")
 title(ylab="Density of TNR",line=7,cex.lab=1.2)
 title(xlab="True Negative Rate (TNR)",line=3,cex.lab=1.2)
+par(xpd=T)
+text(-0.25,7,"b)",cex=1.5)
+par(xpd=F)
 vioplot::vioplot(cbind(AIC1=corData6$RMSEaic[which(corData6$corrLevel==0.1)],STAN1=corData6$RMSEstan[which(corData6$corrLevel==0.1)],AIC5=corData6$RMSEaic[which(corData6$corrLevel==0.5)],STAN5=corData6$RMSEstan[which(corData6$corrLevel==0.5)],AIC9=corData6$RMSEaic[which(corData6$corrLevel==0.9)],STAN9=corData6$RMSEstan[which(corData6$corrLevel==0.9)]), xlab="",
                  horizontal=T,las=1,names=c("Stepwise AIC\n correlation 0.1","Horseshoe\n correlation 0.1","Stepwise AIC\n correlation 0.5","Horseshoe\n correlation 0.5","Stepwise AIC\n correlation 0.9","Horseshoe\n correlation 0.9"),col=c("skyblue","steelblue1","steelblue4","wheat","tan","tan3"),
                  pchMed=20,border=c("royalblue4","royalblue4","royalblue4","tan4","tan4","tan4"),rectCol=c("royalblue4","royalblue4","royalblue4","tan4","tan4","tan4"), lineCol=c("royalblue4","royalblue4","royalblue4","tan4","tan4","tan4"),colMed=c("royalblue4","royalblue4","royalblue4","tan4","tan4","tan4"),ylab="")
 title(ylab="Density of Prediction RMSE",line=7,cex.lab=1.2)
 title(xlab="Prediction Root Mean Square Error (RMSE)",line=3,cex.lab=1.2)
+par(xpd=T)
+text(0.1,7,"c)",cex=1.5)
+par(xpd=F)
 
 dev.off()
 
