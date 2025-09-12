6e64221de3a4917c7996e759706f60767756f940
Dusty-Gannon <71850160+Dusty-Gannon@users.noreply.github.com>
2025-09-12 16:56:20 -0700

    Update the pickle figure

--- Figures/concept_fig2_covariate_shift.R
+++ Figures/concept_fig2_covariate_shift.R
@@ -3,7 +3,7 @@
 # covariate shift with a simple, 2 covariate example.
 #
 # AUTHOR: Dusty Gannon
-# LAST UPDATED: 5 Feb 2025
+# LAST UPDATED: 12 Sept 2025
 ################################################################
 
 # Load required libraries
@@ -27,7 +27,7 @@ construct_main <- function(m, h, tmin, tmax, n, sd) {
   return(data.frame(t = t, y = y))
 }
 
-# Function to construct a time series with a gradient change
+# Function to construct a time series with a gradual change
 construct_grad_2 <- function(t0, m, h, tmin, tmax, n, sd) {
   t <- seq(tmin, tmax, length.out = n)
   a <- m * t0 / (h + t0)
@@ -82,13 +82,15 @@ ts_data <- data.frame(
 
 # Plot the time series with plot annotations
 (ts_plot <- ggplot(ts_data, aes(time, x, color = x_var)) +
-    geom_rect(
-      aes(xmin = 0, xmax = round(n * 0.75), ymin = 0, ymax = 3.5),
-      color = "darkgreen", fill = NA, linewidth = 0.8, linetype = "dashed"
+    annotate(
+      geom = "rect",
+      xmin = 0, xmax = round(n * 0.75), ymin = min(ts_data$x), ymax = 3.5,
+      fill = "darkgreen", alpha = 0.2, color = NA
     ) +
-    geom_rect(
-      aes(xmin = round(n * 0.75) + 1, xmax = n, ymin = 0, ymax = 3.5),
-      color = "darkorange4", fill = NA, linewidth = 1, linetype = "dashed"
+    annotate(
+      geom = "rect",
+      xmin = round(n * 0.75) + 1, xmax = n, ymin = min(ts_data$x), ymax = 3.5,
+      fill = "darkorange4", color = NA, alpha = 0.2
     ) +
     geom_line() +
     theme_classic() +
@@ -97,10 +99,11 @@ ts_data <- data.frame(
       labels = expression(x[1], x[2], x[3])
     ) +
     theme(
-      legend.title = element_blank(),
-      text = element_text(family = "Arial")
+      text = element_text(family = "Arial"),
+      legend.title = element_text(size = 10)
     ) +
-    guides(color = guide_legend(override.aes = list(linewidth = 1)))
+    guides(color = guide_legend(override.aes = list(linewidth = 1))) +
+    labs(color = "Covariate")
 )
 
 ################################################################
@@ -132,7 +135,7 @@ dens_post <- data.frame(
 ################################################################
 
 (dplot_grad_pre <- ggplot(dens_pre, aes(x, y1)) +
-   geom_density2d(color = "darkgreen") +
+   geom_density2d(color = "darkgreen", bins = 7) +
    theme_classic() +
    theme(axis.text = element_blank()) +
    xlab(expression(x[1])) +
@@ -143,7 +146,7 @@ dens_post <- data.frame(
 )
 
 (dplot_grad_post <- ggplot(dens_post, aes(x, y1)) +
-    geom_density2d(color = "darkorange4") +
+    geom_density2d(color = "darkorange4", bins = 7) +
     theme_classic() +
     theme(axis.text = element_blank()) +
     xlab(expression(x[1])) +
@@ -154,7 +157,7 @@ dens_post <- data.frame(
 )
 
 (dplot_abrupt_pre <- ggplot(dens_pre, aes(x, y2)) +
-    geom_density2d(color = "darkgreen") +
+    geom_density2d(color = "darkgreen", bins = 7) +
     theme_classic() +
     theme(axis.text = element_blank()) +
     xlab(expression(x[1])) +
@@ -165,7 +168,7 @@ dens_post <- data.frame(
 )
 
 (dplot_abrupt_post <- ggplot(dens_post, aes(x, y2)) +
-    geom_density2d(color = "darkorange4") +
+    geom_density2d(color = "darkorange4", bins = 7) +
     theme_classic() +
     theme(axis.text = element_blank()) +
     xlab(expression(x[1])) +
@@ -179,20 +182,38 @@ dens_post <- data.frame(
 # Combine plots and save the figure
 ################################################################
 
+l1 <- grid::textGrob(
+  label = "Joint densities during\ntraining period",
+  x = unit(0.55, "npc"), y = unit(0.9, "npc"),
+  gp = grid::gpar(fontsize = 10),
+  vjust = 1
+)
+
+l2 <- grid::textGrob(
+  label = "Joint densities during\ntesting period",
+  x = unit(0.4, "npc"), y = unit(0.9, "npc"),
+  gp = grid::gpar(fontsize = 10, fontfamily = "Arial"),
+  vjust = 1
+)
+
 # Define plot layout
 lo <- "
-AAAAA
-BC#DE
+AAAAAAAAA
+AAAAAAAAA
+AAAAAAAAA
+BBCC#DDEE
+BBCC#DDEE
+FFFF#GGGG
 "
 
 (ts_plot + dplot_grad_pre + dplot_abrupt_pre +
-    dplot_grad_post + dplot_abrupt_post +
+    dplot_grad_post + dplot_abrupt_post + l1 + l2 +
     plot_layout(design = lo))
 
 # Save the plot as a PNG file
 ggsave(
-  here::here("Figures/covariate_shift_concept.png"),
-  device = "png",
-  width = 6, height = 3.5,
-  units = "in"
+  here::here("Figures/covariate_shift_concept.jpg"),
+  device = "jpeg",
+  width = 6, height = 4.5,
+  units = "in", dpi = "retina"
 )
