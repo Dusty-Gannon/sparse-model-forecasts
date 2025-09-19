f14ce2385e8fff457d83bd70398c3a9bb2b5993e
apatt76 <amy.patterson76@gmail.com>
2025-09-19 15:27:55 -0400

    figure 4 edits to align with figure 2

--- Figures/AICvsSTANfigures.R
+++ Figures/AICvsSTANfigures.R
@@ -137,8 +137,8 @@ decorData=read.csv(file="Simulations/AICvsSTANdecorrelation.csv")
 pdf(file=here("Figures/decorrelation_comparison.pdf"), width = 7, height = 8)
 
 
-par(mar=c(5,10,2,2))
-par(mfrow=c(1,1))
+par(mar=c(5,9,2,2))
+par(mfrow=c(3,1))
 
 corr0.9_change=which(decorData$corrLevel==0.9&decorData$corrChange)
 corr0.9_nochange=which(decorData$corrLevel==0.9&!decorData$corrChange)
@@ -147,27 +147,69 @@ corr0.5_nochange=which(decorData$corrLevel==0.5&!decorData$corrChange)
 corr0.1_change=which(decorData$corrLevel==0.1&decorData$corrChange)
 corr0.1_nochange=which(decorData$corrLevel==0.1&!decorData$corrChange)
 
-plot(1, type = "n", xlim = c(0, 12), ylim = c(0.5, 12.5),
-     xaxt = "n", yaxt = "n", xlab = "", ylab = "", bty = "n")
+# plot(1, type = "n", xlim = c(0, 12), ylim = c(0.5, 12.5),
+#      xaxt = "n", yaxt = "n", xlab = "", ylab = "", bty = "n")
 
-rect(xleft=-0.5,ybottom=4.5,xright=13,ytop=8.5,col="gray",border=F)
+# correlation=0.9
+plot(0,0,type="n",ylim=c(0.5,4.5),xlim=c(0.5,12),xlab="",ylab="",xaxt="n",yaxt="n",bty = "n")
 
-vioplot::vioplot(cbind(AICdecorr9=decorData$RMSEaic[corr0.9_change],STANdecorr9=decorData$RMSEstan[corr0.9_change],AICnorm9=decorData$RMSEaic[corr0.9_nochange],STANnorm9=decorData$RMSEstan[corr0.9_nochange],
-                       AICdecorr5=decorData$RMSEaic[corr0.5_change],STANdecorr5=decorData$RMSEstan[corr0.5_change],AICnorm5=decorData$RMSEaic[corr0.5_nochange],STANnorm5=decorData$RMSEstan[corr0.5_nochange],
-                       AICdecorr1=decorData$RMSEaic[corr0.1_change],STANdecorr1=decorData$RMSEstan[corr0.1_change],AICnorm1=decorData$RMSEaic[corr0.1_nochange],STANnorm1=decorData$RMSEstan[corr0.1_nochange]),las=1,cex.axis=0.9,
-                 names=c("Stepwise AIC\n decorrelated 0.9","RHS, decorrelated,\n correlation 0.9","Stepwise AIC, no shift,\n correlation 0.9","RHS, no shift,\n correlation 0.9",
-                         "Stepwise AIC\n decorrelated 0.5","RHS, decorrelated,\n correlation 0.5","Stepwise AIC, no shift,\n correlation 0.5","RHS, no shift,\n correlation 0.5",
-                         "Stepwise AIC\n decorrelated 0.1","RHS, decorrelated,\n correlation 0.1","Stepwise AIC, no shift,\n correlation 0.1","RHS, no shift,\n correlation 0.1"),
-                 xlab="Root Mean Square Error (RMSE)",horizontal=T,col=c("steelblue1","tan","steelblue4","tan3","steelblue1","tan","steelblue4","tan3","steelblue1","tan","steelblue4","tan3"),
-                 pchMed=20,border=c("royalblue4","tan4","royalblue4","tan4","royalblue4","tan4","royalblue4","tan4","royalblue4","tan4","royalblue4","tan4"),
-                 rectCol=c("royalblue4","tan4","royalblue4","tan4","royalblue4","tan4","royalblue4","tan4","royalblue4","tan4","royalblue4","tan4"),
-                 lineCol=c("royalblue4","tan4","royalblue4","tan4","royalblue4","tan4","royalblue4","tan4","royalblue4","tan4","royalblue4","tan4"),
-                 colMed=c("royalblue4","tan4","royalblue4","tan4","royalblue4","tan4","royalblue4","tan4","royalblue4","tan4","royalblue4","tan4"),ylab="",add=T)
+rect(xleft=-0.5,ybottom=-0.5,xright=13,ytop=2.5,col="gray",border=F)
 
-axis(2, at = 1:12, labels = c("Stepwise AIC\n decorrelated 0.9","RHS, decorrelated,\n correlation 0.9","Stepwise AIC, no shift,\n correlation 0.9","RHS, no shift,\n correlation 0.9",
-                              "Stepwise AIC\n decorrelated 0.5","RHS, decorrelated,\n correlation 0.5","Stepwise AIC, no shift,\n correlation 0.5","RHS, no shift,\n correlation 0.5",
-                              "Stepwise AIC\n decorrelated 0.1","RHS, decorrelated,\n correlation 0.1","Stepwise AIC, no shift,\n correlation 0.1","RHS, no shift,\n correlation 0.1"),las=2)
+vioplot::vioplot(cbind(AICdecorr9=decorData$RMSEaic[corr0.9_change],STANdecorr9=decorData$RMSEstan[corr0.9_change],AICnorm9=decorData$RMSEaic[corr0.9_nochange],STANnorm9=decorData$RMSEstan[corr0.9_nochange]),las=1,cex.axis=0.9,
+                 names=c("Stepwise AIC,\n decorrelated","RHS,\ndecorrelated","Stepwise AIC,\nno shift","RHS,\nno shift"),
+                 xlab="Root Mean Square Error (RMSE)",horizontal=T,ylab="",add=T,
+                 col=c("tan3","steelblue4","tan3","steelblue4"),
+                 border=c("tan4","royalblue4","tan4","royalblue4"),
+                 rectCol=c("tan4","royalblue4","tan4","royalblue4"),
+                 lineCol=c("tan4","royalblue4","tan4","royalblue4"),
+                 colMed=c("tan4","royalblue4","tan4","royalblue4"))
+
 axis(1,at=seq(0,12,by=2),labels=seq(0,12,by=2))
+
+axis(2, at = 1:4, labels = c("Stepwise AIC,\n decorrelated","RHS,\ndecorrelated","Stepwise AIC,\nno shift","RHS,\nno shift"),las=2)
+
+title(ylab="Correlation 0.9",line=7,cex.lab=1.2)
+
+# correlation=0.5
+plot(0,0,type="n",ylim=c(0.5,4.5),xlim=c(0.5,12),xlab="",ylab="",xaxt="n",yaxt="n",bty = "n")
+
+rect(xleft=-0.5,ybottom=-0.5,xright=13,ytop=2.5,col="gray",border=F)
+
+vioplot::vioplot(cbind(AICdecorr5=decorData$RMSEaic[corr0.5_change],STANdecorr5=decorData$RMSEstan[corr0.5_change],AICnorm5=decorData$RMSEaic[corr0.5_nochange],STANnorm5=decorData$RMSEstan[corr0.5_nochange]),las=1,cex.axis=0.9,
+                 names=c("Stepwise AIC,\n decorrelated","RHS,\ndecorrelated","Stepwise AIC,\nno shift","RHS,\nno shift"),
+                 xlab="Root Mean Square Error (RMSE)",horizontal=T,ylab="",add=T,
+                 col=c("tan","steelblue1","tan","steelblue1"),
+                 border=c("tan4","royalblue4","tan4","royalblue4"),
+                 rectCol=c("tan4","royalblue4","tan4","royalblue4"),
+                 lineCol=c("tan4","royalblue4","tan4","royalblue4"),
+                 colMed=c("tan4","royalblue4","tan4","royalblue4"))
+
+axis(1,at=seq(0,12,by=2),labels=seq(0,12,by=2))
+
+axis(2, at = 1:4, labels = c("Stepwise AIC,\n decorrelated","RHS,\ndecorrelated","Stepwise AIC,\nno shift","RHS,\nno shift"),las=2)
+
+title(ylab="Correlation 0.5",line=7,cex.lab=1.2)
+
+# correlation=0.1
+plot(0,0,type="n",ylim=c(0.5,4.5),xlim=c(0.5,12),xlab="",ylab="",xaxt="n",yaxt="n",bty = "n")
+
+rect(xleft=-0.5,ybottom=-0.5,xright=13,ytop=2.5,col="gray",border=F)
+
+vioplot::vioplot(cbind(AICdecorr1=decorData$RMSEaic[corr0.1_change],STANdecorr1=decorData$RMSEstan[corr0.1_change],AICnorm1=decorData$RMSEaic[corr0.1_nochange],STANnorm1=decorData$RMSEstan[corr0.1_nochange]),las=1,cex.axis=0.9,
+                 names=c("Stepwise AIC,\n decorrelated","RHS,\ndecorrelated","Stepwise AIC,\nno shift","RHS,\nno shift"),
+                 xlab="Root Mean Square Error (RMSE)",horizontal=T,ylab="",add=T,
+                 col=c("wheat","skyblue","wheat","skyblue"),
+                 border=c("tan4","royalblue4","tan4","royalblue4"),
+                 rectCol=c("tan4","royalblue4","tan4","royalblue4"),
+                 lineCol=c("tan4","royalblue4","tan4","royalblue4"),
+                 colMed=c("tan4","royalblue4","tan4","royalblue4"))
+
+axis(1,at=seq(0,12,by=2),labels=seq(0,12,by=2))
+
+axis(2, at = 1:4, labels = c("Stepwise AIC,\n decorrelated","RHS,\ndecorrelated","Stepwise AIC,\nno shift","RHS,\nno shift"),las=2)
+
+title(ylab="Correlation 0.1",line=7,cex.lab=1.2)
+
 
 
 dev.off()
