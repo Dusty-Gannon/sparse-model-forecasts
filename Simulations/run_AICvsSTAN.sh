1bb023e986cf74a755c19a3c7cbfb46000007261
Dustin G Gannon <dgannon@mblog2.arcc.uwyo.edu>
2026-09-04 17:06:02 -0600

    Added some modules and changed email

--- Simulations/run_AICvsSTAN.sh
+++ Simulations/run_AICvsSTAN.sh
@@ -14,9 +14,9 @@
 #SBATCH --array=1-108
 
 
-module load arcc/1.0 gcc/14.2.0 r/4.4.0
+module load arcc/1.0 gcc/14.2.0 r/4.4.0 r-mass/7.3-59
 
-cd /project/rangeecoevomodels/apatte12/sparse-model-forecasts
+cd /project/modelscape/analyses/sparse-model-forecasts
 
 config=Simulations/AICvsStanConfig.txt
 
